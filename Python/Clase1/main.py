8c6d0b16de5df1a1070056bb43f249135fb3d359
Lautaro Martinez <lautimartinezapooo@gmail.com>
2025-08-19 05:06:36 -0300

    Update main.py

--- Python/Clase1/main.py
+++ Python/Clase1/main.py
@@ -70,18 +70,5 @@ cocina = tuple(cocinaLista)
 print('\n' , cocina)
 #del cocina
 
-# tipo set
-planetas = {'Tierra', 'Marte', 'Venus', 'Jupiter'}
-print(len(planetas)) # Usamos len para ver cuantos elementos tiene = length significa largo 
 
-# Revisar si un elemento existe dentro de set 
-print('Tierra' in planetas)  # Devuelve True si existe, False si no
-print('Saturno' in planetas)  # Devuelve False
 
-# Agregar un elemento al set
-planetas.add('Saturno')
-planetas.add('tierra')
-print(planetas)  # 'tierra' no se agregará porque los sets son únicos
-
-# Eliminar elementos, puede arrojar un error si el elemento no existe 
-planetas.remove("Jupiter")
