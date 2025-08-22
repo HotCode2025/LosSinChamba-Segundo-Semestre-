faf7cf09ed91b98dd512c17710049d95be3a84e8
Leandro Orozco <orozcolean@gmail.com>
2025-08-22 15:08:26 -0300

    Agrego Dest

--- Java/SegundoSemestre/Leccion2/CicloWhile/src/ciclowhile/EjercicioWhile01.java
+++ Java/SegundoSemestre/Leccion2/CicloWhile/src/ciclowhile/EjercicioWhile01.java
@@ -10,15 +10,38 @@ public class EjercicioWhile01 {
         }
         
         var contador = 0;
-    do {
-       System.out.println("contador = " + contador);
-       contador++;
-    }  while (contador <= 7);
-    for (var contando = 0 ; contando < 7; contador++){
-         System.out.println("contando = " + contando);
+        do{
+        System.out.println("contador = " + contador);
+        contador++;
+        }while(contador <= 7);
+        
+        //Uso de las palabras break y continue junto a las etiquetas (labels) 
+        
+        for(var contando = 0; contando < 7; contando++) {
+            if(contando % 2 == 0){
+               System.out.println("contando = " + contando);
+               break;
+            }
+        }  
+        
+        
+        inicio:
+        for(var contando = 0; contando < 7; contando++) {
+            if(contando % 2 != 0){
+                continue inicio ; //Vamos a la siguiente iteración
+            }
+            System.out.println("contando = " + contando);
+        }
+        //Etiquetas Labels
+        
+        for(var contando = 0; contando < 7; contando++) {
+            if(contando % 2 == 0){
+               System.out.println("contando = " + contando);
+               
+            }
+        }    
         
     }
-  }   
 }
            
    
