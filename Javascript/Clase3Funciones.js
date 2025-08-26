4e30e217e425d0533c05277d42e941b6b58c1888
H. Gabriel Máculus <104532543+hgmaculus@users.noreply.github.com>
2025-08-26 16:58:54 -0300

    Clase 3 parte 5

--- Javascript/Clase3Funciones.js
+++ Javascript/Clase3Funciones.js
@@ -10,6 +10,7 @@ function miFuncion(a, b) {
 // llamando la funcion
 miFuncion(5, 4)
 
+// Clase 3 parte 2
 let resultado = miFuncion(6, 7)
 console.log(resultado)
 
@@ -17,3 +18,23 @@ console.log(resultado)
 let x = function(a, b) { return a+b };
 let resultado2 = x(5, 6)
 console.log(resultado2)
+
+// Clase 3 parte 4
+// Funciones de tipo self e invoking
+(function(a, b){
+  console.log("Ejecutando la funcion: " + (a + b))
+})(9, 6)
+
+console.log(typeof miFuncion)
+
+// Clase 3 parte 5
+function miFuncion2(a, b) {
+  console.log(arguments.length)
+}
+
+miFuncion2(5, 7, 3, 6)
+
+// toString
+var miFuncionTexto = miFuncion2.toString()
+console.log(miFuncionTexto)
+
