23d6da4e185eb41337c6ef167d4310a83afc8a71
H. Gabriel Máculus <104532543+hgmaculus@users.noreply.github.com>
2025-08-26 10:24:31 -0300

    Clase 3 video 2 completado

--- Javascript/Clase3Funciones.js
+++ Javascript/Clase3Funciones.js
@@ -3,10 +3,12 @@
 // miFuncion(8, 2)
 
 function miFuncion(a, b) {
-  console.log("Sumamos: " + (a + b))
+  //console.log("Sumamos: " + (a + b))
+  return a + b
 }
 
 // llamando la funcion
 miFuncion(5, 4)
 
-
+let resultado = miFuncion(6, 7)
+console.log(resultado)
