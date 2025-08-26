c3cb3e08de1f83584f1b6e4389d004b8e8470a8b
FacundoBengolea <facundobengolea27e@gmail.com>
2025-08-26 20:31:25 -0300

    Update Clase3Funciones.js

--- Javascript/Clase3Funciones.js
+++ Javascript/Clase3Funciones.js
@@ -39,6 +39,47 @@ var miFuncionTexto = miFuncion2.toString()
 console.log(miFuncionTexto)
 
 // Clase 3 parte 6
+//funciones flecha 
+const sumarFuncionFlecha= (a,b)=>a + b;
+resultado= sumarFuncionFlecha(3,7);
+console.log(resultado);
+//clase 3 parte 7 
+let sumar = function(a = 4,b =8 ){
+    console.log(arguments[0]);
+    console.log(arguments[1]);
+    return a + b + arguments[2];
+}
+resultado = sumar(3, 2, 9);
+console.log(resultado);
+//clase 3 parte 8
 
+let respuesta  =sumarTodo(5, 4, 13, 10, 9);
+console.log(respuesta);
+function sumarTodo(){
+    let suma = 0;
+    for( let i = 0; i < arguments.length; i++){
+        suma += arguments[i];
+    }
+    return suma;
+}
+//clase 3 parte 9
+let k = 10;
+function cambiarValor(a){
+    a = 20;
+}
+cambiarValor(k);
+console.log(k);
+//clase 3 parte 10
+const persona = {
+    nombre: "Juan",
+    apellido: "lopez"
+}
+console.log(persona)
+function cambiarValorObjeto(p1){
+    p1.nombre = "Ignacio";
+    p1.apellido = "Perez";
+}
+cambiarValorObjeto(persona);
+console.log(persona)
 
 
