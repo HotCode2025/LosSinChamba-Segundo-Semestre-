b7f630b3d53545c3a0fcb09a31bf70e5e7db5ab2
H. Gabriel Máculus <gabrielmaculus@gmail.com>
2025-09-09 09:44:59 -0300

    agrego: 6.6_Funciones_paso_argumentos.py

---
+++ Python/clase-5/Clase 5 Funciones Parte 2 Tarea/6.6_Funciones_paso_argumentos.py
@@ -0,0 +1,9 @@
+# Paso de Argumentos (funciones)
+def mi_funcion2(name, lastName):
+    print("Saludos a todos lo que ven a través del canal de YouTube")
+    print(f'Nombre: {name}, Apellido: {lastName}')
+
+mi_funcion2('Jorge', 'Lucero')
+mi_funcion2('Ariel', 'Betancud')
+mi_funcion2('Analia', 'Pedrosa')
+
