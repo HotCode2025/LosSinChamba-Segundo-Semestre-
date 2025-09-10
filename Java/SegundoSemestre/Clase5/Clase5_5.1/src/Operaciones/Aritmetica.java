d3c783587c35957e65fe1bcb3c04e42b0a8e35d0
H. Gabriel Máculus <gabrielmaculus@gmail.com>
2025-09-10 13:45:49 -0300

    agrego: Clase4_5.1 Java

---
+++ Java/SegundoSemestre/Clase5/Clase5_5.1/src/Operaciones/Aritmetica.java
@@ -0,0 +1,9 @@
+package Operaciones;
+
+public class Aritmetica {
+    int a,b;
+    public void sumarNumeros() {
+        int resultado = a + b;
+        System.out.println("Resultado= " + resultado);
+    }
+}
---
+++ Java/SegundoSemestre/Clase5/Clase5_5.1/src/clase5_5/pkg1/Clase5_51.java
@@ -0,0 +1,10 @@
+
+package clase5_5.pkg1;
+
+public class Clase5_51 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
