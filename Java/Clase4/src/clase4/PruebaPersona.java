065c6ea3d5b9400b931f92d6738f70b304c72573
H. Gabriel Máculus <gabrielmaculus@gmail.com>
2025-09-17 22:14:00 -0300

    Clase 4.2: java

---
+++ Java/Clase4/src/clase4/PruebaPersona.java
@@ -0,0 +1,10 @@
+package clase4;
+
+public class PruebaPersona {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        persona1.nombre="Gabriel";
+        persona1.nombre="Apellido";
+        persona1.obtenerInformacion();
+    }
+}
