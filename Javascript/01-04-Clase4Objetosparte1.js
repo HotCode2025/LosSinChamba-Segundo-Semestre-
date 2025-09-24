c5499e75f140aeaa49bd26e2a7eb4c258d9cffc4
Kevin Castilla <kevinrasgido73@gmail.com>
2025-09-24 12:35:55 -0300

    Update 01-04-Clase4Objetosparte1.js

--- Javascript/01-04-Clase4Objetosparte1.js
+++ Javascript/01-04-Clase4Objetosparte1.js
@@ -7,7 +7,7 @@ let persona = {
     apellido : 'castilla',
     email: 'kevinrasgido73@gmail.com',
     edad: 29,
-    idioma: 'ES',
+    idioma: 'ES', //ponemos el idioma en español
     get lang(){
         return this.idioma.toUpperCase(); //convierte las minisculas a mayusculas
     },
@@ -67,4 +67,5 @@ console.log(personaArray);
 console.log('Distintas formas de imprimir un objeto forma 4');
 let personaString = JSON.stringify(persona);
 console.log(personaString);
+
 
