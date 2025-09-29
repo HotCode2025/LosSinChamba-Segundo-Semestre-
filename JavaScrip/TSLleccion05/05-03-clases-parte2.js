6287698c5f31ab657e108d1c903b4c7ae483c0f9
Lautaro Martinez <lautimartinezapooo@gmail.com>
2025-09-29 04:11:38 -0300

    hice la clase 6 en mi rama

---
+++ JavaScrip/TSLleccion05/05-03-clases-parte2.js
@@ -0,0 +1,12 @@
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+let persona1 = new Persona('Leo', 'Messi');
+console.log(persona1);
+
+let persona2 = new Persona('Cristiano', 'Ronaldo');
+console.log(persona2);
