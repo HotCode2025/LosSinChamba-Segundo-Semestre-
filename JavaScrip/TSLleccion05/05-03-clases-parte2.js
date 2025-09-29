c01618b64da8fa98a4ddccc3e55ab28ab35997ad
Lautaro Martinez <lautimartinezapooo@gmail.com>
2025-09-29 04:13:26 -0300

    ahora si

--- JavaScrip/TSLleccion05/05-03-clases-parte2.js
+++ JavaScrip/TSLleccion05/05-03-clases-parte2.js
@@ -6,7 +6,7 @@ class Persona{
 }
 
 let persona1 = new Persona('Leo', 'Messi');
-console.log(persona1);
+console.log(persona1); // Persona { nombre: 'Leo', apellido: 'Messi' }
 
 let persona2 = new Persona('Cristiano', 'Ronaldo');
-console.log(persona2);
+console.log(persona2); // Persona { nombre: 'Cristiano', apellido: 'Ronaldo' }
